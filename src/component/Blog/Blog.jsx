4cbae87ec7768500e9759260213795d0650aaa30
taherul77 <taherul2317@gmail.com>
2023-04-12 23:06:08 +0600

    update

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -38,7 +38,7 @@ const Blog = () => {
                 </div>
 
             </div>
-            <section className="dark:bg-gray-800 dark:text-gray-100">
+            <section className="bg-white">
                 <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
                     
                     <div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-700">
